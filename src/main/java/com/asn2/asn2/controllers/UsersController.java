d1eedb87ffb8996563dbd68b9eae7d977f9aeb5b
stokely <stokelydiamantis@gmail.com>
2023-06-13 13:12:53 -0700

    functional example from vids

---
+++ src/main/java/com/asn2/asn2/controllers/UsersController.java
@@ -0,0 +1,45 @@
+package com.asn2.asn2.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.asn2.asn2.models.User;
+import com.asn2.asn2.models.UserRepository;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class UsersController {  
+    
+    @Autowired
+    private UserRepository userRepo;
+
+    @GetMapping("/users/view")
+    public String getAllUsers(Model model){
+        System.out.println("Getting all users");
+        // get all users from database
+        List<User> users = userRepo.findAll();
+        // end of database call
+        model.addAttribute("us", users);
+        return "users/showAll";
+    }
+
+    @PostMapping("/users/add")
+    public String addUser(@RequestParam Map<String, String> newuser, HttpServletResponse response){
+        System.out.println("ADD user");
+        String newName = newuser.get("name");
+        String newPwd = newuser.get("password");
+        int newSize = Integer.parseInt(newuser.get("size"));
+        userRepo.save(new User(newName,newPwd,newSize));
+        response.setStatus(201);
+        return "users/addedUser";
+    }
+}
