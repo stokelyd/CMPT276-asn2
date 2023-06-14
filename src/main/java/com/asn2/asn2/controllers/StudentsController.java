195595c43c8a0d725bc5e0110df8d081cd45b615
stokely <stokelydiamantis@gmail.com>
2023-06-13 21:19:22 -0700

    basic edit functionality

--- src/main/java/com/asn2/asn2/controllers/StudentsController.java
+++ src/main/java/com/asn2/asn2/controllers/StudentsController.java
@@ -61,24 +61,76 @@ public class StudentsController {
         return "redirect:view";
     }
 
+    @PostMapping("/students/edit")
+    public String editUser(@RequestParam Map<String, String> newStudent, HttpServletResponse response, Model model){
+        /* Serve an Edit view for the requested student uid */
+        System.out.println("EDIT student");
+
+        // String newName = newStudent.get("name"); 
+        // int newWeight = Integer.parseInt(newStudent.get("weight"));
+        // int newHeight = Integer.parseInt(newStudent.get("height"));
+        // String newHairColor = newStudent.get("hairColor"); 
+        // int newGPA = Math.round(10 * Float.parseFloat(newStudent.get("GPA")));
+        // String newMajor = newStudent.get("major");
+
+        // studentRepo.save(new Student(newName,newWeight,newHeight,newHairColor,newGPA,newMajor));
+        // response.setStatus(201); // object created
+        // return "redirect:view";
+
+        List<Student> students = studentRepo.findByUid(Integer.parseInt(newStudent.get("uid")));
+        Student student = students.get(0);
+
+        System.out.println(student.getUid());
+        
+        model.addAttribute("st", student);
+        // return "students/showAll";
+        return "students/editStudent";
+    }
+
     @PostMapping("/students/update")
-    public String editUser(@RequestParam Map<String, String> newStudent, HttpServletResponse response){
+    public String updateUser(@RequestParam Map<String, String> newStudent, HttpServletResponse response){
         // TODO: update given user entry in db
         
         System.out.println("Trying to update user...");
         System.out.println("at uid=" + newStudent.get("uid"));
 
+        // List<Student> students = studentRepo.findByUid(Integer.parseInt(newStudent.get("uid")));
+        // Student student = students.get(0);
+
+        String newName = newStudent.get("name"); 
+        int newWeight = Integer.parseInt(newStudent.get("weight"));
+        int newHeight = Integer.parseInt(newStudent.get("height"));
+        String newHairColor = newStudent.get("hairColor"); 
+        int newGPA = Math.round(10 * Float.parseFloat(newStudent.get("GPA")));
+        String newMajor = newStudent.get("major");
+
+        System.out.println(newName);
+        System.out.println(newWeight);
+        System.out.println(newHeight);
+        System.out.println(newGPA);
+        System.out.println(newMajor);
+
         List<Student> students = studentRepo.findByUid(Integer.parseInt(newStudent.get("uid")));
         Student student = students.get(0);
 
-        System.out.println(student.getName());
-        System.out.println(student.getUid());
-
-        student.setName("Test 2");
-        System.out.println(student.getName());
+        student.setName(newName);
+        student.setWeight(newWeight);
+        student.setHeight(newHeight);
+        student.setHairColor(newHairColor);
+        student.setGPA(newGPA);
+        student.setMajor(newMajor);
 
         studentRepo.save(student);
-        System.out.println(student.getUid());
+
+        // System.out.println(student.getName());
+        // System.out.println(student.getUid());
+
+        // student.setName("Test 2");
+        // System.out.println(student.getName());
+
+        // studentRepo.save(new Student(newName,newWeight,newHeight,newHairColor,newGPA,newMajor));
+
+        // System.out.println(student.getUid());
 
         return "redirect:view";
     }
--- src/main/java/com/asn2/asn2/models/StudentRepository.java
+++ src/main/java/com/asn2/asn2/models/StudentRepository.java
@@ -6,8 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student,Integer> {
     List<Student> findByUid(int uid);
+    
+    // TODO: delete these?
     List<Student> findByHeight(int height);
     List<Student> findByWeight(int weight);
     List<Student> findByName(String name);
-    // List<User> findByNameAndPassword(String name, String password);
 }
