192e7aca3dfe40a50545daa30e3cbb7290926023
stokely <stokelydiamantis@gmail.com>
2023-06-13 23:39:47 -0700

    working final

--- src/main/java/com/asn2/asn2/models/StudentRepository.java
+++ src/main/java/com/asn2/asn2/models/StudentRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student,Integer> {
     List<Student> findByUid(int uid);
-    
-    // TODO: delete these?
     List<Student> findByHeight(int height);
     List<Student> findByWeight(int weight);
     List<Student> findByName(String name);
